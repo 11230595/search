3aa43dc890b0165b52ea103c3914cbb511910bc4
zhoudong <11230595@qq.com>
2018-12-10 14:00:37 +0800

    first commit

---
+++ search/src/main/java/com/zhou/search/service/SolrService.java
@@ -0,0 +1,23 @@
+package com.zhou.search.service;
+
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+
+import java.io.IOException;
+
+/**
+ * @author zhoudong
+ * @version 0.1
+ * @date 2018/12/01 18:06
+ */
+public interface SolrService {
+    /**
+     * solr 查询
+     */
+    SolrDocumentList querySolr(String collection, String q, String field) throws IOException, SolrServerException;
+    /**
+     * 根据ID查询
+     */
+    SolrDocument getById(String collection, String id) throws IOException, SolrServerException;
+}
